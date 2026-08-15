5e7f2f3de51156778d8fe30ba48d3bdca11534b2
안동선(Andy) <dongsun1115@hotmail.com>
2026-08-15 17:59:23 +0900

    feat: 권한 거부 시 나타나는 WindowGroup 추가

--- CookieDungeon/CookieDungeonApp.swift
+++ CookieDungeon/CookieDungeonApp.swift
@@ -10,24 +10,21 @@ import SwiftUI
 @main
 struct CookieDungeonApp: App {
 
-    @State private var appModel = AppModel()
+    @State private var appState = AppState()
 
     var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//                .environment(appModel)
-//        }
-
-        ImmersiveSpace(id: appModel.immersiveSpaceID) {
-            ImmersiveView()
-                .environment(appModel)
-                .onAppear {
-                    appModel.immersiveSpaceState = .open
-                }
-                .onDisappear {
-                    appModel.immersiveSpaceState = .closed
-                }
+        ImmersiveSpace(id: "tutorial-room") {
+            TutorialRoomView()
+                .environment(appState)
         }
         .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        
+        WindowGroup(id: "permission-denied") {
+            PermissionRerequestView()
+                .environment(appState)
+                .frame(minWidth: 1000, maxWidth: 1500, minHeight: 400, maxHeight: 600)
+        }
+        .defaultSize(width: 1000, height: 600)
+        .windowResizability(.contentSize)
     }
 }
