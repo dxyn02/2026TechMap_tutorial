79942017b4d17a8b6fac3fab2a419ae8c13b277e
안동선(Andy) <dongsun1115@hotmail.com>
2026-08-19 16:13:47 +0900

    fix: 불필요한 중복 코드 제거

--- CookieDungeon/AppState.swift
+++ CookieDungeon/AppState.swift
@@ -104,9 +104,7 @@ class AppState {
                 print("쿠키 위치: \(worldPosition)")
                 
                 await placeCookie(at: worldPosition, for: roomAnchor.id)
-                
-                guard let roomMeshResource = roomAnchor.geometry.asMeshResource() else { continue }
-                
+                                
                 if update.event == .added {
                     let roomEntity = ModelEntity(mesh: roomMeshResource, materials: [occlusionMaterial])
                     roomEntity.transform = Transform(matrix: roomAnchor.originFromAnchorTransform)
